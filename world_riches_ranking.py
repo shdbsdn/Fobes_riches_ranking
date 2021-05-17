e22edca941cc8d5e70a4143e24ed3daa356831db
pnu-55118 <83638849+pnu-55118@users.noreply.github.com>
2021-05-17 22:56:26 +0900

    Add files via upload

---
+++ world_riches_ranking.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+
+url = 'https://www.forbes.com/?sh=352d50a52254'
+options=webdriver.ChromeOptions()
+#options.add_argument('headless')
+driver = webdriver.Chrome('C:/Users/82104/chromedriver.exe',options=options)
+driver.get(url)
+
+click_menu=driver.find_element_by_xpath('/html/body/div[1]/header/nav/div[1]/button[1]')
+click_menu.click()
+click_billion=driver.find_element_by_xpath('/html/body/div[1]/header/nav/div[3]/ul/li[1]')
+click_billion.click()
+click_world=driver.find_element_by_xpath('/html/body/div[1]/header/nav/div[3]/ul/li[1]/div[2]/ul/li[2]/a')
+click_world.click()
+data = [[],[]]
+name=driver.find_elements_by_class_name('personName')
+k = 0
+print(len(name))
+for i in name:
+    if k == 0:
+        k += 1
+        print('#')
+        continue
+    print(i.text)
+    data[0].append(i.text)
+    if k == 10:
+        break
+    k += 1
+
+k = 0
+net_worth=driver.find_elements_by_class_name('netWorth')
+print(len(net_worth))
+
+for i in net_worth:
+    if k == 0:
+        k += 1
+        print('#')
+        continue
+    print(float(i.text.replace('$','').replace('B',''))*1000000000)
+    data[1].append(float(i.text.replace('$','').replace('B',''))*1000000000)
+    if k == 10:
+        break
+    k += 1
+print(data)
+
